56b4d286ee855bf47bf7f2198d7b7153c8ad5d90
Jordan Walker <jwalker117@gmail.com>
2020-10-01 23:14:55 -0500

    added more user validation for password matching

--- script.js
+++ script.js
@@ -6,14 +6,54 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordMatch = false;
 
 const validateForm = () => {
     // Using Constraint API
     isValid = form.checkValidity();
     // Style main message for error
-    message.textContent = 'Please fill out all fields';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if(!isValid) {
+        message.textContent = 'Please fill out all fields';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        //adding return statements at this point will save on processing speed
+        return;
+    }
+    // Check to see if passwords match
+    if(password1El.value === password2El.value) {
+        passwordMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else {
+        passwordMatch = false;
+        message.textContent = 'Make sure passwords match.'
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        //adding return statements at this point will save on processing speed
+        return;
+    }
+    // isValid is true and passwords match
+    if(isValid && passwordMatch) {
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+        //Submit data to backend
+        storeFormData();
+    }  
+}
+
+// Store form data as an object to be sent across to the backend
+const storeFormData = () => {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+    console.log(user);
 }
 
 const processFormData = (e) => {
